dc4d66445a1dca0b8c9d6af8fdbcae7b288f4692
Tulasicharan <324103310006@gvpce.ac.in>
2026-08-11 18:15:34 +0530

    ReplaceElementswithGreatestElementRightSide.java

---
+++ ReplaceElementswithGreatestElementRightSide.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int[] replaceElements(int[] arr) 
+    {
+        int n= arr.length;
+        int max_right=-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            int current = arr[i];
+            arr[i]=max_right;
+            max_right=Math.max(max_right,current);
+        }        
+        return arr;
+    }
+}
